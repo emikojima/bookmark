a5fa94187a970e4e9743b63f67d0f565b0f1c8f4
Emi Ko <emi8emi@gmail.com>
2018-12-03 12:19:36 -0800

    scaffold user, book, list

---
+++ db/migrate/20181203201834_create_books.rb
@@ -0,0 +1,12 @@
+class CreateBooks < ActiveRecord::Migration[5.2]
+  def change
+    create_table :books do |t|
+      t.string :title
+      t.string :description
+      t.string :image
+      t.string :notes
+
+      t.timestamps
+    end
+  end
+end
