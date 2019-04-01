2b010f1e0077f6c85c4255788370b933cecaa91e
Emi Ko <emi8emi@gmail.com>
2019-04-01 12:07:36 -0700

    Add redux dev tool capability to index.js

--- client/src/index.js
+++ client/src/index.js
@@ -5,13 +5,13 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import './index.css';
 import { Provider } from 'react-redux';
-import {createStore, applyMiddleware} from "redux";
+import {createStore, applyMiddleware, compose} from "redux";
 import reducer from './store/reducer';
 import thunk from 'redux-thunk';
 
 
-
-const store = createStore(reducer, applyMiddleware(thunk));
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(reducer, composeEnhancers(applyMiddleware(thunk)));
 
 ReactDOM.render(
   <Provider store={store}>
